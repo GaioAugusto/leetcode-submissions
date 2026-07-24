3af4b54136e857e4989cfd8959fc4d7ea4f4c6d2
Gaio Augusto Moraes dos Santos <107768722+GaioAugusto@users.noreply.github.com>
2026-07-23 22:58:05 -0400

    Time: 1 ms (44.41%) | Memory: 19.3 MB (30.48%) - LeetSync

---
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -0,0 +1,8 @@
+import heapq
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        heapq.heapify(nums)
+        while len(nums) > 2:
+            heapq.heappop(nums)
+        return (nums[0]-1)*(nums[1]-1)
